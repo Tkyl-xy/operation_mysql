2fd39aa33c89d3b814ab013e795f8446f38ac9c2
Tkyl-xy <1142085116@qq.com>
2019-01-15 14:15:00 +0800

    在show.py中新增标签，让代码美观一点，在views.py中实现在html中用表单的信息向数据库中新增数据

--- web/views.py
+++ web/views.py
@@ -15,6 +15,14 @@ def login(request):
 
 
 def show(request):
+	if request.method == 'POST':
+		#增加数据
+		a_user = request.POST['a_user']
+		a_pwd = request.POST['a_pwd']
+		models.UserInfo.objects.create(user=a_user, opwd=a_pwd)
+
+
+
 	user_list = models.UserInfo.objects.all() #取出该表所有的数据
 
 	return render(request, 'show.html', {'user_list' : user_list})
