9217a180221daa3bc632f9fa8025f145083a2b90
Tkyl-xy <1142085116@qq.com>
2019-01-15 14:27:53 +0800

    在views.py中新增其他两种增加数据库的方式

--- web/views.py
+++ web/views.py
@@ -16,11 +16,18 @@ def login(request):
 
 def show(request):
 	if request.method == 'POST':
-		#增加数据
 		a_user = request.POST['a_user']
 		a_pwd = request.POST['a_pwd']
-		models.UserInfo.objects.create(user=a_user, opwd=a_pwd)
+		#第一种增加数据方式
+		# models.UserInfo.objects.create(user=a_user, opwd=a_pwd)
 
+		#第二种增加数据方式
+		# obj = models.UserInfo(user=a_user, opwd=a_pwd)
+		# obj.save()
+
+		#第三种增加数据的方式
+		dic = {'user':a_user, 'opwd':a_pwd}
+		models.UserInfo.objects.create(**dic)
 
 
 	user_list = models.UserInfo.objects.all() #取出该表所有的数据
